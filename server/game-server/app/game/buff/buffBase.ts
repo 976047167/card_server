f0820be18d3a2390a0e9662a32cd9149872d99f1
976047167 <976047167@qq.com>
2019-07-18 14:06:02 +0800

    buffbase

--- server/game-server/app/game/buff/buffBase.ts
+++ server/game-server/app/game/buff/buffBase.ts
@@ -1,3 +1,27 @@
+export enum BUFF_TYPE {
+    CONTINUOUS, // 持续性生效的
+}
 export default class BuffBase {
-
+    /**
+     * 是否可以被驱散
+     */
+    public get canBeDispeled() {
+        return false;
+    }
+    public get type() {
+        return this._type;
+    }
+    public get isDebuff() {
+        return false;
+    }
+    /**
+     * 是否可以被同名叠加
+     */
+    public get isOverlayable() {
+        return true;
+    }
+    private _type: BUFF_TYPE;
+    constructor(info) {
+        //
+    }
 }
