5aa5ca257fa77e7c063566605b3c077a0a79b780
976047167 <976047167@qq.com>
2019-07-23 18:04:58 +0800

    fix

--- server/game-server/app/game/trriger.ts
+++ server/game-server/app/game/trriger.ts
@@ -9,7 +9,9 @@ export enum TRRIGER_PRIORITY {
     HIGH,
 }
 export default class Trriger {
-    private _trrigerMap: { [bid: number]: {[timePoint: number]: Array<(args: any) => void>}};
+    private _trrigerMap: { [bid: number]: {[timePoint: number]: number[]}};
+    private _tid: number = 0;
+    private _tidMap: {[tid: number]: (args: any) => void};
     public register(obj: BattleObject|number, timePoint: TIME_POINT, effect: (args: any) => void) {
         if (typeof(obj) !== "number") {
             obj = obj.bId;
@@ -20,16 +22,30 @@ export default class Trriger {
         if (!this._trrigerMap[obj][timePoint]) {
             this._trrigerMap[obj][timePoint] = [];
         }
-        this._trrigerMap[obj][timePoint].push(effect);
+        this._tid++;
+        this._trrigerMap[obj][timePoint].push(this._tid);
+        this._tidMap[this._tid] = effect;
+        return this._tid;
     }
     public notify(obj: BattleObject|number, timePoint: TIME_POINT, args?) {
         if (typeof(obj) !== "number") {
             obj = obj.bId;
         }
         if (!this._trrigerMap[obj]) { return; }
-        const effects = this._trrigerMap[obj][timePoint];
-        effects.forEach((e) => {
-            e(args);
-        });
+        const tids = this._trrigerMap[obj][timePoint];
+        if (!tids) {return; }
+        for (const key in tids) {
+            if (tids.hasOwnProperty(key)) {
+                const tid = tids[key];
+                if (!this._tidMap[tid]) {
+                    delete(tids[key]);
+                } else {
+                    this._tidMap[tid](args);
+                }
+            }
+        }
+    }
+    public remove(tid: number) {
+        delete (this._tidMap[tid]);
     }
 }
