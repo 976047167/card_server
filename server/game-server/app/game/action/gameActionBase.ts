f7da9d9e7b355ce2e45897a3cc1ae162c2b7704c
976047167 <976047167@qq.com>
2020-06-12 15:49:42 +0800

    修复action 错位的bug

--- server/game-server/app/game/action/gameActionBase.ts
+++ server/game-server/app/game/action/gameActionBase.ts
@@ -4,6 +4,17 @@ import { ACTION_TYPE } from "../constants";
 import Trigger from "../trigger";
 import { ACTION_INDEX } from "./gameActionIndex";
 import GameActionManager, { ACTION_STATE } from "./gameActionManager";
+/**
+ *  GameAction 游戏行为（以后统称GA）
+ *  GA通常由一个BO直接调用或设置后由时点触发。
+ *  GA会产生游戏动画，但不是所有GA都产生游戏动画
+ *  一个GA会导致触发另一个或多个GA。
+ *  一个GA有宣告处理，和处理完成两个时点
+ *  GA每次状态发生改变后，会周知Trigger
+ *  GA处理之前会宣告时点，此时若响应其他GA，那么优先处理响应的那些GA
+ *  宣告处理多条GA时，按序触发并处理，若处理完后原GA被否决，则不再继续触发
+ *  如果GA处理被否决，不会触发处理完成时点
+ */
 export class GameActionBase {
 	public get state (): ACTION_STATE {
 		return this._state;
@@ -39,7 +50,7 @@ export class GameActionBase {
 		// tslint:disable-next-line: prefer-for-of
 		for (let i = 0; i < typeList.length; i++) {
 			if (this instanceof ACTION_INDEX[typeList[i]]) {
-				this._type = i;
+				this._type = parseInt(typeList[i]);
 				return;
 			}
 		}
