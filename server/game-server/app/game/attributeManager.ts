86ccd9c8d9794b0f127f9e9944f317ea33b5fffc
976047167 <976047167@qq.com>
2020-07-28 17:59:14 +0800

    属性模块实现修改

--- server/game-server/app/game/attributeManager.ts
+++ server/game-server/app/game/attributeManager.ts
@@ -1,148 +1,163 @@
-import BattlePlayer, { IPlayerInfo } from "./battlePlayer";
-
-export interface IAttribute {
-	str: number;
-	agi: number;
-	spi: number;
-	sta: number;
-	int: number;
-	per: number;
-	derive?: {
-		initiative: number;
-		immunity: number;
-		tenacious: number;
-		[others: string]: number;
-	};
-}
 export type DecoratorId = number;
-export default class AttributeManager implements IAttribute {
+interface IAttribute {
+	name:string,
+	value:number,
+	derived?:{//如果是衍生属性，则有以下几成员
+		base:string[], //如果是衍生属性，则应该基于其他属性
+		isDirty:boolean,
+		update:(...a)=>void,
+	}
+	effected:Set<IAttribute>, //用于反查关联属性
+}
+export default class AttributeManager {
 	/**
-     * 力量
-     */
-	public get str (): number {
-		return this.baseAttribute.str + this.decorator.str;
+	 * 属性储存map
+	 */
+	private _atrMap:Map<string, IAttribute>;
+	// /**
+	//  * 装饰器handler数组
+	//  */
+	// private _applyMap: {
+	// 	[dId: number]: (attribute: any) => any;
+	// };
+	// private _dId: DecoratorId = 0;
+	constructor () {
+		this._atrMap = new Map();
+		// this.initAttribute(player.getInfo());
+	}
+	// /**
+	//  * 属性装饰器
+	//  */
+	// private get decorator () {
+	// 	let decorator = {
+	// 		agi: 0,
+	// 		int: 0,
+	// 		per: 0,
+	// 		spi: 0,
+	// 		sta: 0,
+	// 		str: 0,
+	// 		derive: {
+	// 			immunity: 0,
+	// 			initiative: 0,
+	// 			tenacious: 0,
+	// 		},
+	// 	};
+	// 	for (const dId in this._applyMap) {
+	// 		if (this._applyMap.hasOwnProperty(dId)) {
+	// 			const handle = this._applyMap[dId];
+	// 			decorator = handle(decorator);
+	// 		}
+	// 	}
+	// 	return decorator;
+	// }
+
+	// /**
+	//  * 添加装饰器
+	//  * @param apply 装饰器handler
+	//  */
+	// public registerAtrributeHandle (apply: (arg: any) => any): DecoratorId {
+	// 	this._dId++;
+	// 	this._applyMap[this._dId] = apply;
+	// 	return this._dId;
+	// }
+	// /**
+	//  * 移除装饰器
+	//  * @param did 装饰器Id
+	//  */
+	// public removeAttributeHandle (did: DecoratorId) {
+	// 	if (this._applyMap[did]) {
+	// 		delete this._applyMap[did];
+	// 	}
+	// }
+
+	// private initAttribute (info: IPlayerInfo) {
+	// 	console.log("initAttribute");
+	// }
+	public get (name:string) {
+		if (!this._atrMap.has(name)) return 0;
+		const a:IAttribute = this._atrMap.get(name);
+		!a.derived || !a.derived.isDirty || a.derived.update();//别问，问就是炫技
+		return a.value;
 	}
 	/**
-     * 敏捷
-     */
-	public get agi (): number {
-		return this.baseAttribute.agi + this.decorator.str;
-	}
-	/**
-     * 耐力
-     */
-	public get sta (): number {
-		return this.baseAttribute.sta + this.decorator.sta;
-	}
-	/**
-     * 感知
-     */
-	public get per (): number {
-		return this.baseAttribute.per + this.decorator.per;
-	}
-	/**
-     * 智力
-     */
-	public get int (): number {
-		return this.baseAttribute.int + this.decorator.int;
-	}
-	/**
-     * 意志
-     */
-	public get spi (): number {
-		return this.baseAttribute.spi + this.decorator.spi;
-	}
-	/**
-     * 衍生属性
-     */
-	public get derive () {
-		const p = this;
-		return {
-			/**
-             * 免疫
-             */
-			get immunity (): number {
-				return p.sta + p.spi;
-			},
-			/**
-             * 强韧
-             */
-			get tenacious (): number {
-				return p.sta + p.spi;
-			},
-			/**
-             * 先攻值
-             */
-			get initiative (): number {
-				return p.per + p.agi;
-			},
-		};
+	 * 设置基础属性值
+	 * @param name 属性名
+	 * @param value 值
+	 */
+	public setBaseAtr (name:string, value:number) {
+		let a:IAttribute;
+		if (!this._atrMap.has(name)) {
+			a = {
+				name,
+				value,
+				effected:new Set()
+			};
+			this._atrMap.set(name, a);
+		} else {
+			a = this._atrMap.get(name);
+		}
+		if (a.derived) {
+			console.warn("can't set base attribute");
+			return;
+		}
+		a.value = value;
+		a.effected.forEach((b)=>{
+			b.derived.isDirty = true;
+		});
 	}
 
 	/**
-     * 属性装饰器
-     */
-	private get decorator () {
-		let decorator: IAttribute = {
-			agi: 0,
-			int: 0,
-			per: 0,
-			spi: 0,
-			sta: 0,
-			str: 0,
-			derive: {
-				immunity: 0,
-				initiative: 0,
-				tenacious: 0,
-			},
-		};
-		for (const dId in this._applyMap) {
-			if (this._applyMap.hasOwnProperty(dId)) {
-				const handle = this._applyMap[dId];
-				decorator = handle(decorator);
+	 * 添加衍生属性
+	 * @param name 属性名
+	 * @param base 衍生属性所依赖的基础属性名列表
+	 * @param cal 以基础属性为参数，计算衍生属性值的算式
+	 */
+	public addDerivedAtr (name:string, base:string[], cal:string) {
+		let a:IAttribute;
+		if (this._atrMap.has(name)) {
+			a = this._atrMap.get(name);
+			if (!a.derived) {
+				console.warn(`add derived attribute faild,this name '${name}' has already beeen used in base attribute!`);
+				return;
 			}
-		}
-		return decorator;
-	}
-	private baseAttribute: IAttribute;
-	/**
-     * 装饰器handler数组
-     */
-	private _applyMap: {
-		[dId: number]: (attribute: IAttribute) => IAttribute;
-	};
-	private _dId: DecoratorId = 0;
-	constructor (player: BattlePlayer) {
-		this.initAttribute(player.getInfo());
-	}
-	/**
-     * 添加装饰器
-     * @param apply 装饰器handler
-     */
-	public registerAtrributeHandle (apply: (arg: IAttribute) => IAttribute): DecoratorId {
-		this._dId++;
-		this._applyMap[this._dId] = apply;
-		return this._dId;
-	}
-	/**
-     * 移除装饰器
-     * @param did 装饰器Id
-     */
-	public removeAttributeHandle (did: DecoratorId) {
-		if (this._applyMap[did]) {
-			delete this._applyMap[did];
-		}
-	}
+			a.derived.base.forEach((s)=>{
+				const ia = this._atrMap.get(s);
+				ia.effected.delete(a);
+			});
+		} else {
+			a = {
+				name,
+				value:0,
+				effected:new Set(),
+				derived:{
+					isDirty:false,
+					base,
+					update:null
+				}
 
-	private initAttribute (info: IPlayerInfo) {
-		this.baseAttribute = {
-			agi: info.attribute.agile,
-			int: info.attribute.intellect,
-			per: info.attribute.perception,
-			spi: info.attribute.spirit,
-			sta: info.attribute.stamina,
-			str: info.attribute.strength,
+			};
+			this._atrMap.set(name, a);
+		}
+
+		//给基础属性添加衍生关联
+		base.forEach((baseName)=>{
+			if (!this._atrMap.has(baseName)) {
+				this.setBaseAtr(baseName, 0);
+			}
+			const sa = this._atrMap.get(baseName);
+			sa.effected.add(a);
+		});
+		//生成update方法并调用
+		const func = eval(`(args)=>{return ${cal}}`);
+		const update = ()=> {
+			const values = a.derived.base.map((n)=>{
+				return this.get(n);
+			});
+			a.value = func(values);
+			a.derived.isDirty = false;
 		};
-		console.log("initAttribute", this.baseAttribute);
+		a.derived.update = update;
+		a.derived.update();
+
 	}
 }
