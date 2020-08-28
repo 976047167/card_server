23073141eacd6717af05fe167c1230903d05408f
976047167 <976047167@qq.com>
2020-08-28 16:37:22 +0800

    save

--- server/Logic/libs/CSEval.cs
+++ server/Logic/libs/CSEval.cs
@@ -1,161 +1,213 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace Utils
 
 {
-	delegate double cal(List<double> arg);
+	delegate double CalDel(List<double> arg);
 	/// <summary>
 	/// 解析字符串为委托
 	/// </summary>
 	public class CSEval
 	{
-
-		/// <summary>
-		/// 获取括号中的计算表达式
-		/// （递归）
-		/// </summary>
-		/// <param name="express"></param>
-		public static void GetBraceExpress(ref string express)
+		private static Stack transStr(string str)
 		{
-			int leftBraceMaxIndex = -1;
-			IList<int> rightBraceMinIndexs = new List<int>();
-			for (int i = 0; i < express.Length; i++)
+			Stack ret = new Stack();
+			Stack<string> operate = new Stack<string>();
+			for (int i = 0; i < str.Length; i++)
 			{
-				if (express[i].ToString() == "(")
+				char ch = str[i];
+				if (isNumber(ch))
 				{
-					leftBraceMaxIndex = i;
+					StringBuilder a = new StringBuilder(ch);
+					while (i < str.Length)
+					{
+						i++;
+						ch = str[i];
+						if (isNumber(ch))
+						{
+							a.Append(ch);
+						}
+						else
+						{
+							break;
+						}
+					}
+					ret.Push(Double.Parse(a.ToString()));
 				}
-				if (express[i].ToString() == ")")
+				if (ch.ToString() == "{")
 				{
-					rightBraceMinIndexs.Add(i);
+					StringBuilder a = new StringBuilder();
+					while (i < str.Length)
+					{
+						i++;
+						ch = str[i];
+						if (ch.ToString() == "}")
+						{
+							break;
+						}
+						a.Append(ch);
+					}
+					int idx = int.Parse(a.ToString());
+					CalDel s = (List<double> arg) =>
+					{
+						return arg[idx];
+					};
+					ret.Push(s);
 				}
-			}
-			if (rightBraceMinIndexs.Count == 0) return;
-			int rightBraceIndex = 0;
-			foreach (var item in rightBraceMinIndexs)
-			{
-				if (item > leftBraceMaxIndex)
+				string op = ch.ToString();
+				if (isOperate(op))
 				{
-					rightBraceIndex = item;
-					break;
+					if (operate.Count == 0)
+					{
+						operate.Push(op);
+					}
+					else
+					{
+						string top = operate.Pop();
+						if (top == "(")
+						{
+							operate.Push(top);
+							operate.Push(op);
+						}
+						else
+						{
+							if (compareOperate(op, top))
+							{
+								operate.Push(top);
+								operate.Push(op);
+							}
+							else
+							{
+								ret.Push(top);
+								i--;
+								continue;
+							}
+						}
+
+					}
+
+				}
+				if (op == "(")
+				{
+					operate.Push(op);
+				}
+				if (op == ")")
+				{
+					while (operate.Count > 0)
+					{
+						string a = operate.Pop();
+						if (a == "(")
+						{
+							break;
+						}
+						ret.Push(a);
+					}
 				}
 			}
 
-			string braceExpress = express.Substring(leftBraceMaxIndex, rightBraceIndex - leftBraceMaxIndex + 1);
-			double result = CalcExpress(braceExpress.TrimStart('(').TrimEnd(')')); //计算（）中的表达式
-			express = express.Replace(braceExpress, result.ToString());  //结果替换 （）表达式
-			if (express.IndexOf("(") != -1 && express.IndexOf(")") != -1)
+			while (operate.Count > 0)
 			{
-				GetBraceExpress(ref express);
-				return;
+				object a = operate.Pop();
+				ret.Push(a);
 			}
+			return ret;
 		}
-
-		/// <summary>
-		/// 计算表达式
-		/// </summary>
-		/// <param name="express">表达式</param>
-		/// <returns>表达式结果</returns>
-		public static double CalcExpress(string express)
+		private static bool isNumber(char ch)
 		{
-			List<double> numbers = new List<double>(); //表达式中的数字
-			List<char> operaters = new List<char>();   //表达式中的操作符
-			int tempIndex = 0;
-			for (int i = 0; i < express.Length; i++)
-			{
-				if (!char.IsNumber(express[i]) && char.IsNumber(express[i - 1]) && i > 0)
-				{
-					if (tempIndex != 0)
-						tempIndex = tempIndex + 1;
-					numbers.Add(double.Parse(express.Substring(tempIndex, i - tempIndex)));
-					operaters.Add(express[i]);
-					tempIndex = i;
-				}
-			}
-			numbers.Add(double.Parse(express.Substring(tempIndex + 1, express.Length - tempIndex - 1)));
-			//开始计算
-			double result = 0;
-			if (operaters.Count == 0)
-			{
-				return double.Parse(express);
-			}
-			else
-			{
-				CalcMultiplyDivide(numbers, operaters); //计算乘除
-				result = CalcAddSubduction(numbers, operaters); //计算加减
-			}
-			return result;
+			return char.IsNumber(ch) || ch.ToString() == ".";
+
 		}
-
-		/// <summary>
-		/// 递归计算表达式中的乘/除运算
-		/// </summary>
-		/// <param name="numbers">表达式中的数字集合</param>
-		/// <param name="operaters">操作符集合</param>
-		public static void CalcMultiplyDivide(List<double> numbers, List<char> operaters)
+		private static bool isOperate(string s)
 		{
-			for (int i = 0; i < operaters.Count; i++)
-			{
-				bool temp = false;
-				double n = 0;
-				if (operaters[i] == '*')
-				{
-					n = numbers[i] * numbers[i + 1];
-					temp = true;
-				}
-				else if (operaters[i] == '/')
-				{
-					n = numbers[i] / numbers[i + 1];
-					temp = true;
-				}
-				if (temp)
-				{
-					operaters.RemoveAt(i);
-					numbers.RemoveRange(i, 2);
-					numbers.Insert(i, n);
-					CalcMultiplyDivide(numbers, operaters);
-					break;
-				}
-			}
+			return s == "+" ||
+				s == "-" ||
+				s == "*" ||
+				s == "/";
 		}
-
-		/// <summary>
-		/// 递归计算加减
-		/// </summary>
-		/// <param name="numbers">表达式中的数字集合</param>
-		/// <param name="operaters">操作符集合</param>
-		/// <returns>计算的结果</returns>
-		public static double CalcAddSubduction(List<double> numbers, List<char> operaters)
+		private static bool compareOperate(string s1, string s2)
 		{
-
-			for (int i = 0; i < operaters.Count; i++)
+			return (s1 == "*" || s1 == "/") && (s2 == "+" || s2 == "-");
+		}
+		public static void eval(string str){
+			Stack exps = transStr(str);
+			Stack ret = new Stack();
+			while (exps.Count>0)
 			{
-				bool temp = false;
-				double n = 0;
-				if (operaters[i] == '+')
-				{
-					n = numbers[i] + numbers[i + 1];
-					temp = true;
+				object temp = exps.Pop();
+				if(temp is String ){
+					object a =a
+
+
+				}else{
+					ret.Push(temp);
 				}
-				else if (operaters[i] == '-')
-				{
-					n = numbers[i] - numbers[i + 1];
-					temp = true;
-				}
-				if (temp)
-				{
-					operaters.RemoveAt(i);
-					numbers.RemoveRange(i, 2);
-					numbers.Insert(i, n);
-					CalcAddSubduction(numbers, operaters);
-					break;
-				}
+
+
 			}
-			double result = 0;
-			if (operaters.Count == 0)
-				result = numbers[0];
-			return result;
+
+		}
+		private static double calret(double a,double b ,string operate){
+			if(operate =="+"){
+				return a+b;
+			}
+			if(operate =="-"){
+				return a-b;
+			}
+			if(operate =="*"){
+				return a*b;
+			}
+			if(operate =="/"){
+				return a/b;
+			}
+			return 0;
+		}
+		private static CalDel calret(CalDel a,double b ,string operate){
+			if(operate =="+"){
+				return (List<double> arg) => { return a(arg)+b;};
+			}
+			if(operate =="-"){
+				return (List<double> arg) => { return a(arg)-b;};
+			}
+			if(operate =="*"){
+				return (List<double> arg) => { return a(arg)*b;};
+			}
+			if(operate =="/"){
+				return (List<double> arg) => { return a(arg)/b;};
+			}
+			return a;
+		}
+		private static CalDel calret(double b,CalDel a ,string operate){
+			if(operate =="+"){
+				return (List<double> arg) => { return a(arg)+b;};
+			}
+			if(operate =="-"){
+				return (List<double> arg) => { return b- a(arg);};
+			}
+			if(operate =="*"){
+				return (List<double> arg) => { return a(arg)*b;};
+			}
+			if(operate =="/"){
+				return (List<double> arg) => { return b/a(arg);};
+			}
+			return a;
+		}
+		private static CalDel calret(CalDel a,CalDel b ,string operate){
+			if(operate =="+"){
+				return (List<double> arg) => { return a(arg)+b(arg);};
+			}
+			if(operate =="-"){
+				return (List<double> arg) => { return a(arg)-b(arg);};
+			}
+			if(operate =="*"){
+				return (List<double> arg) => { return a(arg)*b(arg);};
+			}
+			if(operate =="/"){
+				return (List<double> arg) => { return a(arg)/b(arg);};
+			}
+			return a;
 		}
 	}
 }
