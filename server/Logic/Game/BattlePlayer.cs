c2e32d4ba7f1de8b0683fa8a80906659b5a4cba9
976047167 <976047167@qq.com>
2020-08-31 15:38:16 +0800

    save

--- server/Logic/Game/BattlePlayer.cs
+++ server/Logic/Game/BattlePlayer.cs
@@ -18,26 +18,27 @@ namespace Logic
 			this.uid = info.uid;
 
 		}
-		public void gameStart(){
+		public void gameStart()
+		{
 
 		}
-		/**
- * 行动结束
- */
+		/// <summary>
+		/// 行动结束
+		/// </summary>
 		public void turnEnd()
 		{
 			AcitonArg a;
-			a.creator =this.bId;
+			a.creator = this.bId;
 			a.target = 0;
-			this.GAM.pushAction(ACTION_TYPE.TURN_END,a);
+			this.GAM.pushAction(ACTION_TYPE.TURN_END, a);
 		}
 		public void turnBegin()
 		{
-			System.Console.WriteLine("Player:{1} turn begins!",this.uid);
+			System.Console.WriteLine("Player:{1} turn begins!", this.uid);
 			AcitonArg a;
-			a.creator =this.bId;
+			a.creator = this.bId;
 			a.target = 0;
-			this.GAM.pushAction(ACTION_TYPE.TURN_BEGIN,a);
+			this.GAM.pushAction(ACTION_TYPE.TURN_BEGIN, a);
 		}
 
 	}
--- server/Logic/Game/GameController.cs
+++ server/Logic/Game/GameController.cs
@@ -6,37 +6,52 @@ namespace Logic
 	class GameController
 	{
 		private static GameController _instance;
-		private Dictionary<string, object> battleMap;
+		private Dictionary<string, Battle> battleMap;
 		private GameController()
 		{
-			this.battleMap = new Dictionary<string, object>();
+			this.battleMap = new Dictionary<string, Battle>();
 		}
 		public static GameController getInstance()
 		{
 			if (_instance == null) { _instance = new GameController(); }
 			return _instance;
 		}
-		public object createBattle(float seed)
+		public string createBattle(int seed)
 		{
-			return 1;
+			Battle battle = new Battle(seed);
+			this.battleMap.TryAdd(battle.id, battle);
+			return battle.id;
+		}
+		public void startBattle(string battleId)
+		{
+			Battle b = this.getBattle(battleId);
+			b.start();
 		}
 		public void setPlayer(string battleId, PlayerInfo[] playerInfos)
 		{
-
+			Battle b = this.getBattle(battleId);
+			b.setPlayers(playerInfos);
+			System.Console.WriteLine("Battle:{0} setPlayerInfos!", battleId);
 		}
 
 		public void command(UserCommand command)
 		{
-
+			Battle battle = this.getBattle(command.battleId);
+			if (battle == null) { return; }
+			BattlePlayer player = battle.getPlayer(command.uid);
+			if (player == null) { return; }
+			battle.command(command);
 		}
 		public void getSituation(string battleId)
 		{
 
 		}
 
-		public object getBattle(string battleId)
+		public Battle getBattle(string battleId)
 		{
-			return this.battleMap[battleId];
+			Battle ret;
+			this.battleMap.TryGetValue(battleId, out ret);
+			return ret;
 		}
 	}
 
